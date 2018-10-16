941f2e361d5a0eebfe0ec97bd5c3ac7f69996fbc
igorfranca1 <42757976+igorfranca1@users.noreply.github.com>
2018-10-16 14:41:33 -0300

    Add files via upload

---
+++ P3.c
@@ -0,0 +1,21 @@
+/* P3.c */
+/* Igor_Olympio_França */
+/* 11721EEL021 */
+
+#include <stdio.h>
+
+int main()
+{
+	int conv=0, cum=0, i=0;
+	char numero[256];
+	scanf("%s", numero);
+	getchar();
+	for (i=0; numero[i]; i++)
+	{
+		if ((numero[i]-'0')>=0 && (numero[i]-'0')<=9)
+			cum=cum*10+(numero[i]-'0');
+		else;
+	}
+	printf("%i", cum);
+	return 0;
+}
